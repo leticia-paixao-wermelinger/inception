5d2165f57c4589b8170de622bd049055f74b1914
Letícia Paixão <leticiapwermelinger@gmail.com>
2025-11-17 09:14:56 -0300

    Connecting ngingx to wordpress - still with bugs

--- project/srcs/requirements/wordpress/conf/wp-config-create.sh
+++ project/srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
-cd /var/www/
+cd /var/www;
+
+chown -R 33:33 /var/www/html;
+
+# Cria wp-config.php se ainda não existir
 if [ ! -f "/var/www/wp-config.php" ]; then
-  wp cli update
+  wp cli --debug update
   /usr/local/bin/wp config create --dbname="${WORDPRESS_DB_NAME}" --dbuser="${WORDPRESS_DB_USER}" --dbpass="${WORDPRESS_DB_PASSWORD}" --dbhost="${WORDPRESS_DB_HOST}" --force
-  /usr/local/bin/wp config set FS_METHOD 'direct'
+  /usr/local/bin/wp --debug config set FS_METHOD 'direct'
   
-  /usr/local/bin/wp core install --url="https://${WORDPRESS_HOST}" --title="${WORDPRESS_TITLE}" --admin_user="${WORDPRESS_ADM_NAME}" --admin_password="${WORDPRESS_ADM_PASS}" --admin_email="${WORDPRESS_ADM_EMAIL}"
-  /usr/local/bin/wp user create "${WORDPRESS_USERNAME}" "${WORDPRESS_USER_EMAIL}" --role="editor" --user_pass="${WORDPRESS_USER_PASS}"
-fi
+  /usr/local/bin/wp --debug core install --url="https://${WORDPRESS_HOST}" --title="${WORDPRESS_TITLE}" --admin_user="${WORDPRESS_ADM_NAME}" --admin_password="${WORDPRESS_ADM_PASS}" --admin_email="${WORDPRESS_ADM_EMAIL}"
+  /usr/local/bin/wp --debug user create "${WORDPRESS_USERNAME}" "${WORDPRESS_USER_EMAIL}" --role="editor" --user_pass="${WORDPRESS_USER_PASS}"
+fi; 
+
+# echo "Starting PHP-FPM..."
+
+/usr/sbin/php-fpm83 -F
