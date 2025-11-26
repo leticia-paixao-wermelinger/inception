1a3cc4efaf77c1b9c9e4a5f2e38979c69c5c22a1
Letícia Paixão <leticiapwermelinger@gmail.com>
2025-11-26 08:58:50 -0300

    Testes para resolver erro de conexão nginx e wordpress

--- project/srcs/requirements/wordpress/conf/wp-config-create.sh
+++ project/srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
 cd /var/www;
 
-chown -R 33:33 /var/www/html;
+# Altera recursivamente o dono do diretório /var/www/html para o usuário/grupo UID 33.
+# chown -R 33:33 /var/www/html;
 
 # Cria wp-config.php se ainda não existir
-if [ ! -f "/var/www/wp-config.php" ]; then
-  wp cli --debug update
-  /usr/local/bin/wp config create --dbname="${WORDPRESS_DB_NAME}" --dbuser="${WORDPRESS_DB_USER}" --dbpass="${WORDPRESS_DB_PASSWORD}" --dbhost="${WORDPRESS_DB_HOST}" --force
-  /usr/local/bin/wp --debug config set FS_METHOD 'direct'
-  
-  /usr/local/bin/wp --debug core install --url="https://${WORDPRESS_HOST}" --title="${WORDPRESS_TITLE}" --admin_user="${WORDPRESS_ADM_NAME}" --admin_password="${WORDPRESS_ADM_PASS}" --admin_email="${WORDPRESS_ADM_EMAIL}"
-  /usr/local/bin/wp --debug user create "${WORDPRESS_USERNAME}" "${WORDPRESS_USER_EMAIL}" --role="editor" --user_pass="${WORDPRESS_USER_PASS}"
+#if [ ! -f "/usr/local/bin/wp-config.php" ]; then
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+  # Atualiza o WP-CLI para a versão mais recente.
+  # --debug ativa saída detalhada para logs.
+  wp cli --allow-root --debug update
+  # Cria o arquivo wp-config.php com as variáveis do banco.
+  # --force garante sobrescrita se já existir.
+  /usr/local/bin/wp config create --allow-root --dbname="${WORDPRESS_DB_NAME}" --dbuser="${WORDPRESS_DB_USER}" --dbpass="${WORDPRESS_DB_PASSWORD}" --dbhost="${WORDPRESS_DB_HOST}" --force
+  # Adiciona ao wp-config.php: define('FS_METHOD', 'direct'); Isso permite instalações de plugins sem FTP.
+  /usr/local/bin/wp --allow-root --debug config set FS_METHOD 'direct'
+  # Instala efetivamente o WordPress (cria admin e define a URL).
+  /usr/local/bin/wp --allow-root --debug core install --url="https://${WORDPRESS_HOST}" --title="${WORDPRESS_TITLE}" --admin_user="${WORDPRESS_ADM_NAME}" --admin_password="${WORDPRESS_ADM_PASS}" --admin_email="${WORDPRESS_ADM_EMAIL}"
+  /usr/local/bin/wp --debug --allow-root user create "${WORDPRESS_USERNAME}" "${WORDPRESS_USER_EMAIL}" --role="editor" --user_pass="${WORDPRESS_USER_PASS}"
+# Fecha o bloco condicional.
 fi; 
 
 # echo "Starting PHP-FPM..."
 
-/usr/sbin/php-fpm83 -F
+# Inicia o PHP-FPM em foreground (primeiro plano (-F)) - (modo que o Docker precisa para manter o container vivo).
+#/usr/sbin/php-fpm83 -F
