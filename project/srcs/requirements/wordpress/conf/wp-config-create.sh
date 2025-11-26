7a04011919432209d72e8333693f2075755bead1
leticia <leticiapwermelinger@gmail.com>
2025-11-26 12:49:32 -0300

    Ajuste na conf do nginx e inicio de documentacao do mesmo

--- project/srcs/requirements/wordpress/conf/wp-config-create.sh
+++ project/srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -9,15 +9,15 @@ cd /var/www;
 if [ ! -f "/var/www/html/wp-config.php" ]; then
   # Atualiza o WP-CLI para a versão mais recente.
   # --debug ativa saída detalhada para logs.
-  wp cli --allow-root --debug update
+  wp cli --allow-root update
   # Cria o arquivo wp-config.php com as variáveis do banco.
   # --force garante sobrescrita se já existir.
   /usr/local/bin/wp config create --allow-root --dbname="${WORDPRESS_DB_NAME}" --dbuser="${WORDPRESS_DB_USER}" --dbpass="${WORDPRESS_DB_PASSWORD}" --dbhost="${WORDPRESS_DB_HOST}" --force
   # Adiciona ao wp-config.php: define('FS_METHOD', 'direct'); Isso permite instalações de plugins sem FTP.
-  /usr/local/bin/wp --allow-root --debug config set FS_METHOD 'direct'
+  /usr/local/bin/wp --allow-root config set FS_METHOD 'direct'
   # Instala efetivamente o WordPress (cria admin e define a URL).
-  /usr/local/bin/wp --allow-root --debug core install --url="https://${WORDPRESS_HOST}" --title="${WORDPRESS_TITLE}" --admin_user="${WORDPRESS_ADM_NAME}" --admin_password="${WORDPRESS_ADM_PASS}" --admin_email="${WORDPRESS_ADM_EMAIL}"
-  /usr/local/bin/wp --debug --allow-root user create "${WORDPRESS_USERNAME}" "${WORDPRESS_USER_EMAIL}" --role="editor" --user_pass="${WORDPRESS_USER_PASS}"
+  /usr/local/bin/wp --allow-root core install --url="https://${WORDPRESS_HOST}" --title="${WORDPRESS_TITLE}" --admin_user="${WORDPRESS_ADM_NAME}" --admin_password="${WORDPRESS_ADM_PASS}" --admin_email="${WORDPRESS_ADM_EMAIL}"
+  /usr/local/bin/wp --allow-root user create "${WORDPRESS_USERNAME}" "${WORDPRESS_USER_EMAIL}" --role="editor" --user_pass="${WORDPRESS_USER_PASS}"
 # Fecha o bloco condicional.
 fi; 
 
